527008acd3c0aa39e360b5929976828f15631210
Jekabs-Olukalns <olukalns3@gmail.com>
2023-11-24 14:27:13 +0200

    otrais uzdevums

--- 2.uzdevums.py
+++ 2.uzdevums.py
@@ -1,2 +1,4 @@
 #Izveidojiet Pythonprogrammu, kas izmanto while ciklu, lai atrastu pirmo skaitli, kura kvadrāts ir lielāks par 1000.
-
+skaitlis=[1,2,3,4,5,6,7,8,9,10]
+kvadrati=list(filter(lambda x: x%2==0 or x%3, skaitlis))
+print(kvadrati)
